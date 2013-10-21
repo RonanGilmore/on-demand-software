4628b07c26abd31e5b4ca280bc2a48b33446eb70
RonanGilmore <Ronan.gilmore@fmr.com>
2013-10-21 14:20:24 +0100

    Update ReadEvent.java

--- src/main/java/com/yummynoodlebar/core/events/ReadEvent.java
+++ src/main/java/com/yummynoodlebar/core/events/ReadEvent.java
@@ -1,7 +1,7 @@
 package com.yummynoodlebar.core.events;
 
 public class ReadEvent {
-  protected boolean entityFound = false;
+  protected boolean entityFound = true;
 
   public boolean isEntityFound() {
     return entityFound;
